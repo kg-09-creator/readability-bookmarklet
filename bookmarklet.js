43dad6e8f2324a59f34e9e60aef46763f8c35b29
kg-09-creator <kavyaguptaa@gmail.com>
2026-05-23 14:52:36 -0700

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,5 +1,12 @@
 javascript: (function () {
-    alert('Welcome to my bookmarklet!');
-    document.body.style.backgroundColor = 'lightpink';
-    document.textContent = "white";
+    alert('hallooo welcome to my bookmarklet! hopefully after using this it becomes easier to read the webpage!')
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = 'white';
+    document.body.style.fontSize = '24px';
+    document.body.style.fontFamily = 'sans-serif';
+
+    var imgs=document.querySelectorAll('img');
+    for(var i=0; i<imgs.length; i++) {
+        imgs[i].remove();
+    }
 })();
